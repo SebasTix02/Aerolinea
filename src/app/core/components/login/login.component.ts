ebd6f3c7f7222cc4cf3583d60d99f9973f751b71
Sebastian Tixilema <sebastixi2002@gmail.com>
2024-01-17 20:13:55 -0500

    Implementacion visual registro

--- src/app/core/components/login/login.component.ts
+++ src/app/core/components/login/login.component.ts
@@ -3,6 +3,7 @@ import { FormControl, FormGroup, Validators } from '@angular/forms';
 import { HttpClient } from '@angular/common/http';
 import { Router } from '@angular/router';
 import { AuthService } from './auth.service';
+import { ChangeDetectorRef } from '@angular/core';
 
 @Component({
   selector: 'gad-login',
@@ -10,19 +11,29 @@ import { AuthService } from './auth.service';
   styleUrls: ['./login.component.css'],
 })
 export class LoginComponent {
-  togglePassword = true;
-  group: FormGroup;
   hide = true;
-  urlFlights = '/ServiceUser.svc/Login'
+  isLoginForm = true;
+  group: FormGroup;
+
+  hidePassword = true;
+  hideConfirmPassword = true;
+
   constructor(
     private httpClient: HttpClient,
     private router: Router,
-    private authService: AuthService
+    private authService: AuthService,
+    private cdRef: ChangeDetectorRef,
   ) {
     this.group = new FormGroup({
       user: new FormControl(null, Validators.required),
       password: new FormControl(null, Validators.required),
+      confirmPassword: new FormControl(null), // Agrega este control sin validador
     });
+
+    // Establece el validador solo para el formulario de registro
+    if (!this.isLoginForm) {
+      this.group.get('confirmPassword')?.setValidators([Validators.required]);
+    }
   }
 
   login() {
@@ -34,7 +45,7 @@ export class LoginComponent {
         console.log(response  == true)
         if (response == true) {
           console.log('Inicio de sesión exitoso');
-          this.authService.isLoggedIn = true; // Utiliza el setter para asignar el valor
+          this.authService.isLoggedIn = true; 
           this.router.navigate(['vuelos']);
         } else {
           console.error('Credenciales incorrectas');
@@ -48,5 +59,48 @@ export class LoginComponent {
 
   logout() {
     this.authService.logout();
+  }
+
+  toggleForms() {
+    this.isLoginForm = !this.isLoginForm;
+
+    // Restablece los validadores al cambiar el formulario
+    if (this.isLoginForm) {
+      this.group.get('confirmPassword')?.clearValidators();
+    } else {
+      this.group.get('confirmPassword')?.setValidators([Validators.required]);
+    }
+
+    this.group.get('confirmPassword')?.updateValueAndValidity();
+
+    this.cdRef.detectChanges();
+  }
+
+  toggleHidePassword() {
+    this.hidePassword = !this.hidePassword;
+  }
+
+  toggleHideConfirmPassword() {
+    this.hideConfirmPassword = !this.hideConfirmPassword;
+  }
+
+  registro() {
+    const user = this.group.get('user')?.value;
+    const password = this.group.get('password')?.value;
+    const confirmPassword = this.group.get('confirmPassword')?.value;
+
+    if (this.isLoginForm) {
+      // Aquí implementa la lógica de inicio de sesión
+      console.log('Inicio de sesión', user, password);
+    } else {
+      // Aquí implementa la lógica de registro
+      // Verifica si las contraseñas coinciden
+      if (password !== confirmPassword) {
+        
+        return;
+      }
+
+      console.log('Registro exitoso', user, password);
+    }
   }
 }
